38e3c59e24949deae0c935d12d158cf13001ed59
CI System <sh@signalwerk.ch>
2019-11-13 11:38:39 +0000

    Deploy to GitHub Pages: 6bf2bc1998c347bbbc3ee77cb67e378f3e29700f

---
+++ precache-manifest.483bf58e617c826fcdd113dde6787606.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314abf7956f5efb101fb9c7ef145032b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d965093229c07ea7fdb",
+    "url": "/static/css/main.40191526.chunk.css"
+  },
+  {
+    "revision": "6bf4ac9106ecb68575b4",
+    "url": "/static/js/2.5eb80cbe.chunk.js"
+  },
+  {
+    "revision": "9d965093229c07ea7fdb",
+    "url": "/static/js/main.68205656.chunk.js"
+  },
+  {
+    "revision": "17940367234cd7bf55ac",
+    "url": "/static/js/runtime-main.c306e1da.js"
+  }
+]);
